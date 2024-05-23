8b04d35e392b2815795a9e5a62cffb5f563971d5
Morata91 <mrt.with.kk15@icloud.com>
2024-05-23 16:14:08 +0900

    face_detectを変更
    ・OpenCVからdlibに変更

--- demo/demo.py
+++ demo/demo.py
@@ -6,10 +6,11 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 
 import cv2
+import dlib
+import torch
 
 
 from models.alexnet_based.MPIITrackerModel import MPIITrackerModel
-import torch
 
 # 画面の解像度を取得する
 screen_width = 2560
@@ -34,6 +35,8 @@ def main():
 
     # 赤い点の位置
     red_dot_position = (screen_width // 2, screen_height // 2)  # 例として画面の中央に設定
+    
+    detector = dlib.get_frontal_face_detector()
 
 
     while True:
@@ -47,7 +50,7 @@ def main():
         # フレームを画面サイズにリサイズする
         frame_resized = cv2.resize(frame, (screen_width, screen_height))
         
-        face_detect(frame_resized)
+        face_detect(detector, frame_resized)
         
         # フレームに赤い点を描画する
         # 第1引数：描画対象の画像（frame_resized）第2引数：円の中心座標（red_dot_position）第3引数：円の半径（例では10ピクセル）第4引数：円の色（BGR形式で指定、例では赤色 (0, 0, 255)）第5引数：円の塗りつぶし（-1で塗りつぶし）
@@ -83,37 +86,19 @@ def predict():
     
     # with torch.no_grad():
     #     x, y, _ = model(imFace, imEyeL, imEyeR, faceGrid, eyeGrid)
-        
-def face_detect(frame_resized):
-    # Haarカスケードの分類器をロード
-    face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
-    eye_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_eye.xml')
     
+def face_detect(detector, frame_resized):
+    # dlibの顔検出器をロード
     # グレースケールに変換
     gray = cv2.cvtColor(frame_resized, cv2.COLOR_BGR2GRAY)
     # 顔検出
-    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    faces = detector(gray)
     
-    for (x, y, w, h) in faces:
-        # 顔部分の切り抜き
-        # face_crop = frame_resized[y:y+h, x:x+w]
-        # cv2.imwrite('face_crop.jpg', face_crop)
-
-        # 検出された顔の中で目を検出
-        roi_gray = gray[y:y+h, x:x+w]
-        roi_color = frame_resized[y:y+h, x:x+w]
-        eyes = eye_cascade.detectMultiScale(roi_gray)
-
-        for (ex, ey, ew, eh) in eyes:
-            # 目部分の切り抜き
-            # eye_crop = roi_color[ey:ey+eh, ex:ex+ew]
-            # cv2.imwrite(f'eye_crop_{ex}_{ey}.jpg', eye_crop)
-
-            # 目のバウンディングボックスを描画
-            cv2.rectangle(frame_resized, (x + ex, y + ey), (x + ex+ew, y + ey+eh), (0, 255, 0), 2)
-
+    for face in faces:
+        x, y, w, h = face.left(), face.top(), face.width(), face.height()
         # 顔のバウンディングボックスを描画
-        cv2.rectangle(frame_resized, (x, y), (x+w, y+h), (255, 0, 0), 2)
+        cv2.rectangle(frame_resized, (x, y), (x + w, y + h), (255, 0, 0), 2)
+
 
 
 
